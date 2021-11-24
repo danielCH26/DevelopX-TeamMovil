ed8f22a2208061b4592c54af921c94387ea7446d
camiloadamss <camiloadamss@gmail.com>
2021-11-23 23:20:10 -0500

    Pantalla de registro, se enlaza en el login desde el texto de crear cuenta, vistas en portrait para todas las pantallas

--- app/src/main/java/com/example/pideloapp/UserRegister.java
+++ app/src/main/java/com/example/pideloapp/UserRegister.java
@@ -3,6 +3,7 @@ package com.example.pideloapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class UserRegister extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class UserRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+    }
+
+    public void readTPYC(View view) {
     }
 }
