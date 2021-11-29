9347ed89747b89b84d759d3d835a6fe8d207dfbc
camiloadamss <camiloadamss@gmail.com>
2021-11-29 16:58:20 -0500

    Vista de registro con Freagmento de Terminos, politicas y condiciones (TPYC), xml de fragmento y actividad java creadas, revisar que se crashea la app al entrar en la pantalla de registro, pero no genera error en la compilacion

--- app/src/main/java/com/example/pideloapp/UserRegister.java
+++ app/src/main/java/com/example/pideloapp/UserRegister.java
@@ -1,18 +1,44 @@
 package com.example.pideloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class UserRegister extends AppCompatActivity {
+
+    FragmentContainerView FragTPYC;
+    Fragment Frag;
+    TextView readTPYC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+        FragTPYC = (FragmentContainerView) findViewById(R.id.FragmentTPYC);
+        TextView readTPYC = (TextView) findViewById(R.id.txvTPYC);
+
+
     }
 
     public void readTPYC(View view) {
+
+        Frag = new TPYCFragment();
+        cargarFragmento(Frag);
+
     }
+
+    private void cargarFragmento(Fragment frag) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.commit();
+    }
+
+
 }
