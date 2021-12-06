2dc782e087c9c4e67cb2440b3a00359162a6cceb
Daniel10 <danielcordobahoward@gmail.com>
2021-12-05 23:06:55 -0500

    Logica activity Welcome cambio en el regisro de usuario, adicion de campo rol

--- app/src/main/java/com/example/pideloapp/Welcome.java
+++ app/src/main/java/com/example/pideloapp/Welcome.java
@@ -1,8 +1,12 @@
 package com.example.pideloapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Welcome extends AppCompatActivity {
 
@@ -12,4 +16,31 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         setSupportActionBar(findViewById(R.id.tlbWelcome));
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_welcome_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionSearch:
+                Toast.makeText(this, "Boton buscar funciona", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.actionSettings:
+                Toast.makeText(this, "Boton configuraciones funciona", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.actionLogout:
+                Toast.makeText(this, "Boton cerrar sesion funciona", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }*/
 }
