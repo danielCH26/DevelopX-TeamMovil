fdfa73c411bf828e5f2a6375e2895bfb03897111
Daniel10 <danielcordobahoward@gmail.com>
2021-12-07 00:40:10 -0500

    Logigica MainActivity para la autenticacion con firebase

--- app/src/main/java/com/example/pideloapp/MainActivity.java
+++ app/src/main/java/com/example/pideloapp/MainActivity.java
@@ -2,18 +2,28 @@ package com.example.pideloapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText EdtUser;
     EditText EdtPassword;
     Button btnLogin, btnJoinIn;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +34,40 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         btnJoinIn = findViewById(R.id.btnGoJoinIn);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
     public void onLogin(View view) {
-        if (EdtUser.getText().toString().equals("daniel") && EdtPassword.getText().toString().equals("1234"))
+
+        String Password = EdtPassword.getText().toString();
+        String Mail = EdtUser.getText().toString();
+
+        if (TextUtils.isEmpty(Mail))
         {
-            Intent ToWelcome = new Intent (view.getContext(),Welcome.class);
-            startActivity(ToWelcome);
-            Toast.makeText(getApplicationContext(), "@string/txtWelcome", Toast.LENGTH_LONG).show();
+            EdtUser.setError("Ingresa un correo");
+            EdtUser.requestFocus();
+        }
+        else if (TextUtils.isEmpty(Password)){
+            Toast.makeText(this,  "Ingresa una contraseña", Toast.LENGTH_SHORT).show();
+            EdtPassword.requestFocus();
         }
         else{
-            Toast.makeText(this,  "@string/txtErrorLogin", Toast.LENGTH_SHORT).show();
+            mAuth.signInWithEmailAndPassword(Mail, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Bienvenid@ a Pidelo", Toast.LENGTH_SHORT).show();
+                        Intent ToWelcome = new Intent (view.getContext(),Welcome.class);
+                        startActivity(ToWelcome);
+                    }
+                    else{
+                        Log.w("TAG","Error",task.getException());
+                    }
+
+                }
+            });
         }
     }
 
