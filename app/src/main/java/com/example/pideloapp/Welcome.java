e01ca5d32dfadf415e2b481feffe5762836d90eb
Daniel10 <danielcordobahoward@gmail.com>
2021-12-07 14:45:27 -0500

    Adicion de logica funcional de logout y boton de perfil en el toolbar para ver la info del usuario

--- app/src/main/java/com/example/pideloapp/Welcome.java
+++ app/src/main/java/com/example/pideloapp/Welcome.java
@@ -4,12 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Welcome extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +40,16 @@ public class Welcome extends AppCompatActivity {
                 Toast.makeText(this, "Boton configuraciones funciona", Toast.LENGTH_SHORT).show();
                 return true;
 
+            case R.id.actionProfile:
+                Toast.makeText(this, "Boton perfil funciona", Toast.LENGTH_SHORT).show();
+                return true;
+
             case R.id.actionLogout:
-                Toast.makeText(this, "Boton cerrar sesion funciona", Toast.LENGTH_SHORT).show();
+
+                mAuth.signOut();
+                Toast.makeText(this, "Cerrando sesion", Toast.LENGTH_SHORT).show();
+                Intent ToLogin = new Intent (this,MainActivity.class);
+                startActivity(ToLogin);
                 return true;
 
             default:
