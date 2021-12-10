ed646d23436d39b93c4e02beb275609685a7b2b1
uncion7 <uncion7@gmail.com>
2021-12-10 04:04:26 -0500

    Logica ingreso de usuario y validacion de identidad con firebase

--- app/src/main/java/com/example/pideloapp/MainActivity.java
+++ app/src/main/java/com/example/pideloapp/MainActivity.java
@@ -1,49 +1,77 @@
 package com.example.pideloapp;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtEmail;
-    EditText edtPass;
+    EditText EdtUser;
+    EditText EdtPassword;
+    Button btnLogin, btnJoinIn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edtEmail = (EditText) findViewById(R.id.edtUser);
-        edtPass = (EditText) findViewById(R.id.edtPassword);
-        Button bntLogin = (Button) findViewById(R.id.btnLogin);
+
+        EdtUser = findViewById(R.id.edtUser);
+        EdtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnJoinIn = findViewById(R.id.btnGoJoinIn);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
     }
 
     public void onLogin(View view) {
-        Toast aviso = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG);
-        if (edtEmail.getText().toString().equals("a@b.c") && edtPass.getText().toString().equals("1234"))
+
+        String Password = EdtPassword.getText().toString();
+        String Mail = EdtUser.getText().toString();
+
+        if (TextUtils.isEmpty(Mail))
         {
-            Intent ToWelcome = new Intent (view.getContext(),Welcome.class);
-            startActivity(ToWelcome);
-            aviso.setText(getString(R.string.txtWelcome));
-            aviso.show();
+            EdtUser.setError("Ingresa un correo");
+            EdtUser.requestFocus();
+        }
+        else if (TextUtils.isEmpty(Password)){
+            Toast.makeText(this,  "Ingresa una contraseña", Toast.LENGTH_SHORT).show();
+            EdtPassword.requestFocus();
         }
         else{
-            aviso.setText(getString(R.string.txtErrorLogin));
-            aviso.show();
+            mAuth.signInWithEmailAndPassword(Mail, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Bienvenid@ a Pidelo", Toast.LENGTH_SHORT).show();
+                        Intent ToWelcome = new Intent (view.getContext(),Welcome.class);
+                        startActivity(ToWelcome);
+                    }
+                    else{
+                        Log.w("TAG","Error",task.getException());
+                    }
+                }
+            });
         }
     }
 
     public void onGoJoinIn(View view) {
-        //Intent ToJoinIn = new Intent (view.getContext(),JoinIn.class);
-        //startActivity(ToJoinIn);
+        Intent ToJoinIn = new Intent (view.getContext(),JoinIn.class);
+        startActivity(ToJoinIn);
     }
-
 }
