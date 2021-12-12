e79fd3bffdc186b40354108b41681e50228a49da
Daniel10 <danielcordobahoward@gmail.com>
2021-12-12 03:45:01 -0500

    Creacion de Fragments funcionales para el funcionamiento de la app

--- app/src/main/java/com/example/pideloapp/Welcome.java
+++ app/src/main/java/com/example/pideloapp/Welcome.java
@@ -3,6 +3,9 @@ package com.example.pideloapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -36,11 +39,28 @@ public class Welcome extends AppCompatActivity {
                 Toast.makeText(this, "Boton buscar funciona", Toast.LENGTH_SHORT).show();
                 return true;
 
+            case R.id.actionCart:
+                replaceFragment(new CartFragment());
+                Toast.makeText(this, "Boton buscar funciona", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.actionHome:
+                replaceFragment(new HomeFragment());
+                Toast.makeText(this, "Boton buscar funciona", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.actionCategories:
+                replaceFragment(new CategoriesFragment());
+                Toast.makeText(this, "Boton configuraciones funciona", Toast.LENGTH_SHORT).show();
+                return true;
+
             case R.id.actionSettings:
+                replaceFragment(new SettingsFragment());
                 Toast.makeText(this, "Boton configuraciones funciona", Toast.LENGTH_SHORT).show();
                 return true;
 
             case R.id.actionProfile:
+                replaceFragment(new ProfileFragment());
                 Toast.makeText(this, "Boton perfil funciona", Toast.LENGTH_SHORT).show();
                 return true;
 
@@ -56,8 +76,14 @@ public class Welcome extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-
     }
 
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+    }
 
 }
